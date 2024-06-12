6ad5ccc2b78e142d160aaae088bafabc2f3f83c3
Suraj Sharma <125715507+surajsharma111@users.noreply.github.com>
2024-06-12 20:11:34 +1000

    Configured backend and database

---
+++ backend/routes/index.js
@@ -0,0 +1,11 @@
+
+import express from 'express';
+
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Zoom' });
+});
+
+export default router
