60066dab7b15460f2216b5f4548e38efcd0aec91
Suraj Sharma <125715507+surajsharma111@users.noreply.github.com>
2024-06-18 11:34:11 +1000

    added zoom logo

--- frontend/src/pages/SignUp.jsx
+++ frontend/src/pages/SignUp.jsx
@@ -1,6 +1,20 @@
+
+
 function SignUp(){
     return(
-        <h2>Sign UP</h2>
+        <>
+           <div className="container">
+                <div className="topbar">
+                    <div>
+                        <img src="../assets/Images/Zoom-Logo.png" alt="" />
+                    </div>
+                </div>
+                <div className="body">
+                    <div className="left-container"></div>
+                    <div className="right-container"> </div>
+                </div>
+           </div>
+        </>
     )
 }
 export default SignUp
